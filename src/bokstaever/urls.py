c50195a8ecf2d8a3b0e0644d186c9d3bf9abd3ee
Till Wesselmann <till@cryptec.at>
2018-05-02 22:35:43 +0200

    index page and base.html for main application

--- src/bokstaever/urls.py
+++ src/bokstaever/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from django.urls import include, path
 
+from bokstaever.views import PostListView
+
+app_name = 'bokstaever'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls'), name='dashboard')
+    path('dashboard/', include('dashboard.urls'), name='dashboard'),
+
+    path('', PostListView.as_view(), name='post-list')
 ]
---
+++ src/bokstaever/views.py
@@ -0,0 +1,9 @@
+from django.views.generic.list import ListView
+
+from bokstaever.models import Post
+
+class PostListView(ListView):
+    model = Post
+
+    template_name = 'bokstaever/index.html'
+    paginate_by = 20
