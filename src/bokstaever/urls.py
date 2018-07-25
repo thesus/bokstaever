a0f2edc64a16c6cc1b568bab860a4bf33cd6076f
Till Wesselmann <till@cryptec.at>
2018-07-25 12:07:36 +0200

    Login/logout views integrated

--- src/bokstaever/urls.py
+++ src/bokstaever/urls.py
@@ -8,12 +8,21 @@ from bokstaever.views import (
     PostDetailView
 )
 
+from django.contrib.auth import views as auth_views
+
 app_name = 'bokstaever'
+
+template = 'registration/form.html'
+authentication = [
+    path('accounts/login/', auth_views.LoginView.as_view(template_name=template)),
+    path('accounts/logout/', auth_views.LogoutView.as_view())
+]
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls'), name='dashboard'),
     path('', PostListView.as_view()),
     path('<int:page>', PostListView.as_view(), name='post-list'),
-    path('post/<slug:slug>', PostDetailView.as_view(), name='post-detail'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('post/<slug:slug>', PostDetailView.as_view(), name='post-detail')
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + authentication
