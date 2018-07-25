d1c3cfc770b0043bad69c8d08291ede56efc7c1d
Till Wesselmann <till@cryptec.at>
2018-07-25 18:30:11 +0200

    Fixed base path and static root in base settings

--- src/bokstaever/conf/settings/base.py
+++ src/bokstaever/conf/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,9 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+print(BASE_DIR)
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = 'static'
 
 STATIC_URL = '/static/'
 
