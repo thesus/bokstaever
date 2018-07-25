f036eb5596360b7c6fa26cbcc9f4e0501077c3ea
Till Wesselmann <till@cryptec.at>
2018-07-25 10:34:27 +0200

    Hyphenation, media rules and bugfix for saving of Post-model

--- src/bokstaever/models.py
+++ src/bokstaever/models.py
@@ -62,8 +62,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200)
 
     def save(self, *args, **kwargs):
-        if not self.url_slug:
-            self.url_slug = slugify(self.headline)
+        if not self.slug:
+            self.slug = slugify(self.headline)
         super().save(*args, **kwargs)
 
     def get_absolute_url(self):
