84d3664b4923b59e4a55da6c2e1b18337bb2a217
Till Wesselmann <till@cryptec.at>
2018-08-01 22:57:54 +0200

    Basic styling for notifications

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="notifications">
-    <div v-for="message in messages">
-      {{ message.text }}
+    <div class="card" v-for="message in messages" :class="message.type">
+      <div class="header">{{ message.title }}</div>
+      <div class="content">{{ message.text }}</div>
     </div>
   </div>
 </template>
@@ -29,5 +30,42 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.notifications {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+  width: 400px;
+
+  display: block;
+
+  .card {
+    margin: 8px 0 8px 0;
+    background-color: pink;
+    .header {
+      height: 20px;
+      padding: 2px 0 2px 5px;
+      border-bottom: 1px solid;
+    }
+    .content {
+      padding: 5px;
+    }
+    &.danger {
+        background-color: rgb(178, 18, 49);
+        color: white;
+        opacity: 0.9;
+      .header {
+         background-color: rgb(148, 15, 40);
+      }
+    }
+    &.success {
+      background-color: rgb(8, 133, 39);
+      color: white;
+      opacity: 0.9;
+      .header {
+        background-color: rgb(7, 112, 33);
+      }
+    }
+  }
+}
 </style>
