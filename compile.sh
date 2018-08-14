b24ea025636b017e2565a6544c721edf26409e9f
Till Wesselmann <till@cryptec.at>
2018-08-14 12:55:11 +0200

    Remove existing builds in compile script

--- compile.sh
+++ compile.sh
@@ -3,9 +3,11 @@
 # This script is for compiling the dashboard in the caddy container with the correct urls
 
 cd /srv/src
+rm dist
 
 yarn
 
 yarn build
 
+rm /srv/dashboard
 mv /srv/src/dist /srv/dashboard
