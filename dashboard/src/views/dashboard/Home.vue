f2920a40ac5ffa60dd0aa0d925e3f1fe51b3ccef
Till Wesselmann <till@cryptec.at>
2018-08-24 16:10:27 +0200

    Removed test button and added reminder

--- dashboard/src/views/dashboard/Home.vue
+++ dashboard/src/views/dashboard/Home.vue
@@ -1,20 +1,7 @@
 <template>
-  <div>
-    <notifications/>
-    <button @click="sendMessage">Send Message</button>
-  </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    sendMessage () {
-      this.$notify({
-        type: 'danger',
-        title: 'Achtung',
-        text: 'Ein Fehler beim Speichern ist aufgetreten! Sie müssen nun aufhören die Anwendung zu benutzen.'
-      })
-    }
-  }
-}
+// TODO(till): Add nice graphics 'n stuff.
+export default {}
 </script>
