33cbc06bc5456ff0c8b7fae4ad8d1569cbe3a37d
Till Wesselmann <till@cryptec.at>
2018-09-18 02:50:36 +0200

    Added vuex-persistedstate that saves the store and window.localStorage

--- dashboard/src/store/index.js
+++ dashboard/src/store/index.js
@@ -3,16 +3,24 @@ import Vuex from 'vuex'
 
 import createLogger from 'vuex/dist/logger'
 
+import createPersistedState from 'vuex-persistedstate'
+
 import authentication from './modules/authentication'
 
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 
+let plugins = [createPersistedState()]
+
+if (debug) {
+  plugins.push(createLogger())
+}
+
 export default new Vuex.Store({
   modules: {
     authentication
   },
   strict: debug,
-  plugins: debug ? [createLogger()] : []
+  plugins: plugins
 })
