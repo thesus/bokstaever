ed4df6e30cb94e8fbb4e3a6308a82cd43245e345
Till Wesselmann <till@cryptec.at>
2018-10-08 13:56:04 +0200

    Destination of theme dependency downloader is now configurable

--- utils/download_theme_dependencies.sh
+++ utils/download_theme_dependencies.sh
@@ -3,6 +3,11 @@
 # This script downloads dependencies needed for the themes
 # Run from repository root (../)
 
+if [ -z "$DESTINATION" ]; then
+    echo "Setting destination to server/bokstaever/static/"
+    DESTINATION=server/bokstaever/static/
+fi
+
 declare -a packages=("https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"
                      "https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"
 )
@@ -10,9 +15,9 @@ declare -a packages=("https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.j
 for i in "${packages[@]}"
 do
     case $i in
-        *.js) destination=server/bokstaever/static/js;;
-        *.css) destination=server/bokstaever/static/css;;
-        * ) echo "\033[0;31m Unknown extension format. Don't know where to put!" && exit 1;;
+        *.js) extension=js;;
+        *.css) extension=css;;
+        * ) echo "Unknown extension format. Don't know where to put!" && exit 1;;
     esac
-    wget -N --content-disposition -P "$destination" "$i"
+    wget -N --content-disposition -P "$DESTINATION$extension" "$i"
 done
