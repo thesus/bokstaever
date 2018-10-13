4623ca34f11d3d6d469ba8d0df86f4317e6ba644
Till Wesselmann <till@cryptec.at>
2018-10-13 16:30:08 +0200

    Remove masonry on mobile devices, layout for posts, including galleries and images

--- server/bokstaever/static/js/frimarke.js
+++ server/bokstaever/static/js/frimarke.js
@@ -1,12 +1,42 @@
-var grid = document.querySelector('.posts');
+var msnry
 
-var msnry = new Masonry( grid, {
-  itemSelector: '.post',
-  columnWidth: 350,
-  gutter: 25,
-  fitWidth: true
-});
+var grid = document.querySelector('.posts')
+
+function createMasonry (grid) {
+  return new Masonry( grid, {
+    itemSelector: '.post',
+    columnWidth: 350,
+    gutter: 25,
+    fitWidth: true
+  }) 
+}
+
+if (window.innerWidth > 500) {
+  msnry = createMasonry(grid)
+} else {
+  msnry = undefined
+}
+
+function checkMasonry () {
+  let width = window.innerWidth
+  if (width <= 500) {
+    if (msnry != undefined) {
+      // console.log('destroying msnry')
+      msnry.destroy()
+      msnry = undefined
+    }
+  } else {
+    if (msnry === undefined) {
+      // console.log('creating msnry')
+      msnry = createMasonry(grid)
+    }
+  }
+}
+
+window.addEventListener('resize', checkMasonry)
 
 imagesLoaded(grid).on( 'progress', function() {
-  msnry.layout()
+  if (msnry) {
+    msnry.layout()
+  }  
 })
