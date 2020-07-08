66d0312f63fa0bdffec426c5f1ddd9a0e857a57b
Till Wesselmann <till@cryptec.at>
2020-07-08 23:26:55 +0200

    Add views for images

--- server/dashboard/urls.py
+++ server/dashboard/urls.py
@@ -1,13 +1,28 @@
 from django.urls import path
 
-from dashboard.views import PostList, PostUpdate, PostCreate, ImageListSimple
+from dashboard.views import (
+    PostList,
+    PostUpdate,
+    PostCreate,
+    PageList,
+    PageUpdate,
+    PageCreate,
+    ImageListSimple,
+    ImageList,
+    ImageCreate,
+)
 
 app_name = "dashboard"
 
 urlpatterns = [
     # path("", )
-    path("posts/", PostList.as_view()),
+    path("posts/", PostList.as_view(), name="post-list"),
     path("posts/<int:pk>/", PostUpdate.as_view(), name="post-edit"),
-    path("posts/create", PostCreate.as_view()),
+    path("posts/create", PostCreate.as_view(), name="post-create"),
+    path("pages/", PageList.as_view(), name="page-list"),
+    path("pages/<int:pk>/", PageUpdate.as_view(), name="page-edit"),
+    path("pages/create", PageCreate.as_view(), name="page-create"),
+    path("images/", ImageList.as_view(), name="image-list"),
+    path("images/create", ImageCreate.as_view(), name="image-create"),
     path("images/simple/", ImageListSimple.as_view()),
 ]
--- server/dashboard/views.py
+++ server/dashboard/views.py
@@ -5,27 +5,21 @@ from django.utils.translation import gettext_lazy as _
 from django.core import serializers
 from django.http import JsonResponse
 
-from bokstaever.models import Post
+from bokstaever.models import (
+    Post,
+    DatabasePage,
+    FilePage,
+    PageModel
+)
 from images.models import Image
 
 from django import forms
 
 from dashboard.fields import ImageChoiceField
 
+
 class DashboardListView(ListView):
     paginate_by = 6
-
-class ImageListSimple(DashboardListView):
-    template_name = "dashboard/image_list_simple.html"
-    model = Image
-
-    def get(self, request, *args, **kwargs):
-        paginator, page, queryset, _ = self.paginate_queryset(self.get_queryset(), self.paginate_by)
-        result = []
-        for entry in queryset:
-            result.append((entry.id, entry.thumbnail.image_file.url))
-
-        return JsonResponse({'result': result, 'current': page.number, 'count': paginator.count, 'pages': paginator.num_pages }, status=200)
 
 
 class PostList(DashboardListView):
@@ -38,21 +32,76 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['headline', 'draft', 'text', 'type', 'image']
+        fields = ["headline", "draft", "text", "type", "image"]
 
-class PostEdit():
+
+class PostEdit:
     template_name = "dashboard/post_edit.html"
     success_url = "/dashboard/posts"
     form_class = PostForm
     model = Post
 
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-
-        return form
 
 class PostUpdate(PostEdit, UpdateView):
     pass
 
+
 class PostCreate(PostEdit, CreateView):
     pass
+
+
+class PageList(DashboardListView):
+    template_name = "dashboard/page_list.html"
+    model = DatabasePage
+
+
+class PageForm(forms.ModelForm):
+    image = ImageChoiceField()
+
+    class Meta:
+        model = DatabasePage
+        fields = ["headline", "show_menu", "text", "type", "draft", "image"]
+
+
+class PageEdit:
+    template_name = "dashboard/page_edit.html"
+    success_url = "/dashboard/pages"
+    form_class = PageForm
+    model = DatabasePage
+
+
+class PageUpdate(PageEdit, UpdateView):
+    pass
+
+
+class PageCreate(PageEdit, CreateView):
+    pass
+
+
+class ImageListSimple(DashboardListView):
+    template_name = "dashboard/image_list_simple.html"
+    model = Image
+
+    def get(self, request, *args, **kwargs):
+        paginator, page, queryset, _ = self.paginate_queryset(
+            self.get_queryset(), self.paginate_by
+        )
+        result = []
+        for entry in queryset:
+            result.append((entry.id, entry.thumbnail.image_file.url))
+
+        return JsonResponse(
+            {
+                "result": result,
+                "current": page.number,
+                "count": paginator.count,
+                "pages": paginator.num_pages,
+            },
+            status=200,
+        )
+
+class ImageList(DashboardListView):
+    template_name = "dashboard/image_list.html"
+    model = Image
+
+class ImageCreate()
