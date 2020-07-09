6c98c738c2df3f94973b5a05eda4f6f5eb425ec6
Till Wesselmann <till@cryptec.at>
2020-07-09 23:29:47 +0200

    Cleanup image upload

--- server/dashboard/static/js/image_upload.js
+++ server/dashboard/static/js/image_upload.js
@@ -17,14 +17,21 @@ class ImageUpload {
 
     this.csrf_token = document.getElementsByName("csrfmiddlewaretoken")[0].value
 
+    // Images extracted from the multi file input
     this.images = []
+    // Amount of images loading, used for actions once after finishing all uploads
     this.loading = 0
+    // Encodes if there was an error on any upload
     this.wasError = false
 
+    // Uploads the images to the server
     this.trigger.onclick = this.uploadImages.bind(this)
+
+    // Updates `this.images` if the input changes
     this.imageInput.onchange = this.updateImages.bind(this)
   }
 
+  // Closes all alerts on the alert anchor
   clearAlert() {
     for (let node of this.alert.childNodes) {
       let alert = new bootstrap.Alert(this.alert.firstChild)
@@ -35,16 +42,16 @@ class ImageUpload {
     }
   }
 
+  // Appends a alert with the given message to the alert anchor
   displayError(message) {
-    let alert = document.createElement("div")
-    alert.className = "alert alert-danger alert-dismissible fade show"
+    let alert = createElement("div", "alert alert-danger alert-dismissible fade show")
     alert.setAttribute("role", "alert")
+
     let content = document.createElement("span")
     content.appendChild( document.createTextNode(message) )
     alert.appendChild(content)
 
-    let button = document.createElement("button")
-    button.className = "close"
+    let button = createElement("button", "close")
     button.type = "button"
     button.setAttribute("data-dismiss", "alert")
     button.setAttribute("aria-label", "close")
@@ -59,6 +66,7 @@ class ImageUpload {
     this.alert.appendChild(alert)
   }
 
+  // Changes visibility of the progress bar of a given image
   toggleProgress(id) {
     let node = this.images[id].node.lastChild.lastChild.lastChild
 
@@ -69,6 +77,7 @@ class ImageUpload {
     }
   }
 
+  // Resets the progress bar to 0
   resetProgress(id) {
     let node = this.images[id].node.lastChild.lastChild.lastChild.lastChild
 
@@ -77,31 +86,39 @@ class ImageUpload {
     node.setAttribute("aria-valuenow", 0)
   }
 
-  toggleInput(id, on) {
+  // Disables the input on a given image
+  disableInput(id, on) {
     let node = this.images[id].node.lastChild.lastChild.firstChild
     node.disabled = on
   }
 
+  // Disables the upload button
   disableSubmit(on) {
     this.trigger.disabled = on
   }
 
   uploadImages() {
+    // count how many requests will be made to the server
     this.loading = this.images.filter((image) => { return (image.success === null || image.success === false) }).length
+    // reset previous error state
     this.wasError = false
+    // Allow pressing upload only once
     this.disableSubmit(true)
+    // reset alert state
     this.clearAlert()
 
     for (const [index, image] of this.images.entries()) {
       if (image.success === null || image.success === false) {
         if (image.success === false) {
+          // Reset the error lining on previously failed images
           image.node.className = "card"
         } else {
-          // Enable progress bar on the given image
+          // Enable progress bar on the given image on the first try
           this.toggleProgress(index)
         }
 
-        this.toggleInput(index, true)
+        // Disable inputs during upload
+        this.disableInput(index, true)
 
         let data = new FormData()
         data.append("image", image.file)
@@ -114,6 +131,7 @@ class ImageUpload {
 
         xhr.upload.addEventListener("progress", this.updateProgressFunction(index).bind(this))
 
+        // Update the image on load and error events
         xhr.addEventListener("load", this.updateImageFunction(index).bind(this))
         xhr.addEventListener("error", this.updateImageFunction(index).bind(this))
 
@@ -121,10 +139,56 @@ class ImageUpload {
         xhr.send(data)
       }
     }
-
-    return false
   }
 
+  // Updates the internal representation via the event listener during upload
+  updateImageFunction(id) {
+    return function (event) {
+      let status = event.target.status === 200
+      this.images[id].success = status
+      // one image less is loading
+      this.loading--
+
+      if (status) {
+        // set green card lining on success
+        this.images[id].node.className = "card border-success"
+      } else {
+        // reset progress and enable input for next attempt
+        this.resetProgress(id)
+        this.disableInput(id, false)
+        this.wasError = true
+
+        // set red card lining on failure
+        this.images[id].node.className = "card border-danger"
+      }
+
+      // if all images are loaded and there was an error, show alert and reenable the upload button
+      if (this.wasError && this.loading === 0) {
+        this.displayError("An error occured. You may try to upload again.")
+        this.disableSubmit(false)
+      }
+    }
+  }
+
+  // update the progress for a given image
+  updateProgressFunction(id) {
+    return function (event) {
+      let node = this.images[id].node.lastChild.lastChild.lastChild.lastChild
+      let value = Math.round((event.loaded / event.total) * 100)
+
+      node.setAttribute("aria-valuenow", value)
+      node.style.width = `${value}%`
+    }
+  }
+
+  // change the internal image title if the input changes
+  updateTitleFunction(id) {
+    return function (event) {
+      this.images[id].title = event.target.value
+    }
+  }
+
+  // Reads the images from the input
   updateImages() {
     this.images = []
 
@@ -144,51 +208,13 @@ class ImageUpload {
       })
     }
 
+    // if there are (new) images enable the upload button
     this.disableSubmit(this.images.length === 0)
 
     this.updateDOM()
   }
 
-  updateImageFunction(id) {
-    return function (event) {
-      let status = event.target.status === 200
-      this.images[id].success = status
-      this.loading--
-
-      // this.toggleProgress(id)
-
-      if (status) {
-        this.images[id].node.className = "card border-success"
-      } else {
-        this.resetProgress(id)
-        this.toggleInput(id, false)
-        this.wasError = true
-
-        this.images[id].node.className = "card border-danger"
-      }
-
-      if (this.wasError && this.loading === 0) {
-        this.displayError("An error occured. You may try to upload again.")
-        this.disableSubmit(false)
-      }
-    }
-  }
-
-  updateProgressFunction(id) {
-    return function (event) {
-      let node = this.images[id].node.lastChild.lastChild.lastChild.lastChild
-      let value = Math.round((event.loaded / event.total) * 100)
-      node.setAttribute("aria-valuenow", value)
-      node.style.width = `${value}%`
-    }
-  }
-
-  updateTitleFunction(id) {
-    return function (event) {
-      this.images[id].title = event.target.value
-    }
-  }
-
+  // creates the image cards after creating the internal representation
   updateDOM() {
     // Clear old images
     anchor.textContent = ""
@@ -197,18 +223,18 @@ class ImageUpload {
       let card = createElement("div", "card")
       card.style.width = 180
 
+      // create Progressbar as last child
       let progress = createElement("div", "progress")
       progress.style.display = "none"
-
       let progressBar = createElement("div", "progress-bar")
       progressBar.setAttribute("role", "progressbar")
-
       // Accessibility
       progressBar.setAttribute("aria-valuenow", 0)
       progressBar.setAttribute("aria-valuemin", 0)
       progressBar.setAttribute("aria-valuemax", 100)
       progress.appendChild(progressBar)
 
+      // Add the image
       let img = createElement('img', "card-img-top")
       img.src = image.url
 
